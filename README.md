944e096bd5a04ebea28991700ab7ac84d4b2aa12
Bo Zhang <bo@gradle.com>
2019-05-23 08:15:08 +0800

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Java程序的基本结构练习：调试器
+
+请在本地运行[`Main`类](https://github.com/hcsp/set-a-condition-breakpoint/blob/master/src/main/java/Main.java)，在指定位置设置断点并进行断点调试，将捕捉到的值填入输入框。
+
+敬请期待写代码啦的课程
+
+敬请期待
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.IOUtils;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.stream.Stream;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,15 +13,22 @@ public class Main {
         }
     }
 
-    private static String readFileContent() throws IOException {
-        Stream<String> entries = Stream.of(System.getProperty("java.class.path").split(File.pathSeparator));
-        File targetClassDir = entries.filter(entry -> entry.endsWith("target/classes")).findFirst().map(File::new).orElseThrow(IllegalStateException::new);
+    public static String readFileContent() throws IOException {
+        Stream<String> entries =
+                Stream.of(System.getProperty("java.class.path").split(File.pathSeparator));
+        File targetClassDir =
+                entries.filter(entry -> entry.endsWith("target/classes"))
+                        .findFirst()
+                        .map(File::new)
+                        .orElseThrow(IllegalStateException::new);
         File mainDotJava = new File(targetClassDir, "../../src/main/java/Main.java");
 
         return IOUtils.toString(new FileReader(mainDotJava)).replaceAll("\\s", "");
     }
 
-    private static String calculate(int i, String fileContent) throws IOException {
+    public static String calculate(int i, String fileContent) throws IOException {
+        // Please set a condition breakpoint with "i==5000" here to catch the value of result
+        // 请在这里设置一个i==5000的条件断点，捕捉result的值
         String result = DigestUtils.md5Hex(fileContent + i);
         return result;
     }
